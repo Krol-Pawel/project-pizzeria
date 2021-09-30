5efdb24dc43f2ba27313bb5d6f6cb49f2d6e7b28
Paweł Król <krol.pawel89@gmail.com>
2021-09-30 15:51:22 +0200

    end task with other file

--- src/js/components/Products.js
+++ src/js/components/Products.js
@@ -1,6 +1,6 @@
-import {select, classNames, templates} from './settings.js';
-import utils from './utils.js';
-import AmountWidget from './components/AmountWidget.js';
+import {select, classNames, templates} from '../settings.js';
+import utils from '../utils.js';
+import AmountWidget from './AmountWidget.js';
 
 class Product{
   constructor(id, data){
@@ -184,13 +184,16 @@ class Product{
   addToCart(){
     const thisProduct = this;
 
+    thisProduct.name = thisProduct.data.name;
+    thisProduct.amount = thisProduct.amountWidget.value;
+
     //app.cart.add(thisProduct.prepareCartProduct());
 
     const event = new CustomEvent('add-to-cart', {
       bubbles: true,
       detail: {
         product: thisProduct,
-      }
+      },
     });
 
     thisProduct.element.dispatchEvent(event);
